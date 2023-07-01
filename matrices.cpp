7dba86bf111c4eda6bda2d9728beed49548321fb
Gustav Häger <gustav.hager@gmail.com>
2023-07-01 10:54:19 +0200

    Fix compiler settings

--- matrices.cpp
+++ matrices.cpp
@@ -5,7 +5,7 @@ using namespace Catch;
 using namespace Eigen;
 
 TEST_CASE("NormalizeFeatures", "[Matrix]"){
-  MatrixXf featureMatrix(32, 1024*512);
+  MatrixXf featureMatrix = MatrixXf::Random(4, 512*512);
 
   BENCHMARK("Inplace"){
     for(int i=0; i < featureMatrix.cols(); i++){
@@ -15,7 +15,7 @@ TEST_CASE("NormalizeFeatures", "[Matrix]"){
   };
 
   BENCHMARK("Not inplace"){
-    MatrixXf normalizedFeatures(32, 1024*512);
+    MatrixXf normalizedFeatures(4, 512*512);
     for(int i=0; i < featureMatrix.cols(); i++){
       normalizedFeatures.col(i) = featureMatrix.col(i).normalized();
     }
@@ -23,11 +23,35 @@ TEST_CASE("NormalizeFeatures", "[Matrix]"){
   };
 
   BENCHMARK("Semi Handwritten"){
-    MatrixXf out(32, 1024*512);
+    MatrixXf out(4, 1024*512);
     for(int i=0; i < featureMatrix.cols(); i++){
       float colsum = featureMatrix.array().sqrt().col(i).sum();
       out.col(i) = featureMatrix.col(i) / colsum;
     }
     return out;
+  };
+
+  BENCHMARK("Handwritten"){
+	MatrixXf normalizedFeatureMatrix(4, 512*512);
+	float* inPtr = featureMatrix.data();
+	float* outPtr = normalizedFeatureMatrix.data();
+	size_t numElements = 512*512;
+	
+	for(size_t i=0; i < numElements; i++){
+	  float sum = 0;
+
+	  for(size_t j=0; j < 4; j++){
+	    sum += std::pow(inPtr[j], 2.f);
+	  }
+	  
+	  for(size_t j=0; j < 4; j++){
+            outPtr[j] = inPtr[j] / std::sqrt(sum);
+	  }
+
+	  inPtr += 4;
+	  outPtr += 4;
+	}
+
+	return normalizedFeatureMatrix;
   };
 }
